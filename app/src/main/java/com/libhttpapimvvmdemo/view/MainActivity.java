e1d858bb8c4dde73ad6b9d2453402bf2855a584a
mazwu110 <315145320@qq.com>
2019-12-26 15:11:40 +0800

    Add files via upload
    首页

--- app/src/main/java/com/libhttpapimvvmdemo/view/MainActivity.java
+++ app/src/main/java/com/libhttpapimvvmdemo/view/MainActivity.java
@@ -5,9 +5,14 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import com.libhttpapimvvmdemo.R;
 import com.libhttpapimvvmdemo.databinding.ActivityMainBinding;
+import com.libhttpapimvvmdemo.javaBean.QTestBean;
+import com.libhttpapimvvmdemo.view.adapter.UserInfoAdapter;
 import com.libhttpapimvvmdemo.view.adapter.WeatherAdapter;
 import com.libhttpapimvvmdemo.viewModel.MainActivityVM;
 import com.qlib.base.BaseActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // 所谓的MVVM模式，M就是获取数据的一个独立类，比如做实际的网络请求等，
 // V就是界面显示的一个类，说白了就是MainActivity类似的界面，VM其实就是
@@ -20,6 +25,7 @@ public class MainActivity extends BaseActivity {
     ActivityMainBinding mBinding;
     MainActivityVM VM;
     WeatherAdapter mAdapter;
+    UserInfoAdapter tAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +38,8 @@ public class MainActivity extends BaseActivity {
     // 其他初始化
     void init() {
         // 注意 mAdapter 顺序，有列表的时候才需要
-        mAdapter = new WeatherAdapter(this);
-        VM = new MainActivityVM(this, mAdapter);
+        mAdapter = new WeatherAdapter(this, R.layout.item_weather);
+        VM = new MainActivityVM(this, mAdapter); // 除了这一句外，其他可以移到MainActivityVM中，以减少Activity的臃肿，这里就不移了，测试代码而已
         // 注意 lambda表达式不支持Java8以下的，所以JDK必须是8及以上的哦，要不会报错
         VM.getTestData().observe(this, data -> mBinding.setUserName(data));
         // 设置监听
@@ -43,6 +49,22 @@ public class MainActivity extends BaseActivity {
         mBinding.recyclerView.setLayoutManager(layoutManager); // 设置recyclerview布局方式
         mBinding.recyclerView.setAdapter(mAdapter); //给recyclerview 添加适配器
 
+        // 这个是超级封装的适配器
+        tAdapter = new UserInfoAdapter (this, R.layout.item_my_weather);
+        LinearLayoutManager layoutManagerT = new LinearLayoutManager(this);
+        mBinding.recyclerViewTest.setLayoutManager(layoutManagerT); // 设置recyclerview布局方式
+        mBinding.recyclerViewTest.setAdapter(tAdapter); //给recyclerview 添加适配器
+
+        List<QTestBean> list = new ArrayList<> ();
+        for (int i = 0; i < 10; i++) {
+            QTestBean bean = new QTestBean();
+            bean.setId ("" + i);
+            bean.setName ("张三" + i);
+            bean.setAge ("" + (20 + i));
+            list.add (bean);
+        }
+
+        tAdapter.addRecordList (list);
     }
 
     @Override
