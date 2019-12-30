8b5dec82ebcd61eef71023caf6d2c801df9b207b
mazwu110 <315145320@qq.com>
2019-12-30 10:22:23 +0800

    Add files via upload
    修改基类

--- qlib/src/main/java/com/qlib/base/BaseViewModel.java
+++ qlib/src/main/java/com/qlib/base/BaseViewModel.java
@@ -2,18 +2,25 @@ package com.qlib.base;
 
 import android.arch.lifecycle.ViewModel;
 import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
+import com.qlib.qutils.ToastUtils;
+
 public class BaseViewModel extends ViewModel {
+    protected Handler myHandler = new Handler(Looper.getMainLooper());
     protected Context mContext;
 
     protected void showToast(String msg) {
         if (TextUtils.isEmpty(msg))
             msg = "请求失败，请稍后再试";
-        Toast.makeText(mContext, msg, Toast.LENGTH_LONG).show();
+
+        final String showMsg = msg;
+        myHandler.post(()->ToastUtils.showToast (showMsg));
     }
 
     // 强制关闭键盘
